75ab06df034a91f68f892ca5be99055fc6ac0f5f
alex-h <alexanderohernandez21@gmail.com>
2022-10-05 19:30:12 -0700

    added logic to add past hours

--- assetes/index.js
+++ assetes/index.js
@@ -1,3 +1,5 @@
+let text = $("textarea");
+
 // displays the header date
 function time() {
     $("#currentDay").text(moment().format("dddd,MMMM Do"));
@@ -5,17 +7,13 @@ function time() {
 
 // checks time for each text box
 function checkTime() {
-    var sum;
-    for(let i = 0; i < $("textarea").length; i++) {
-        sum = 9 + i;
-        
+    for(let i = 0; i < text.length; i++) {
         // gives military time 
-        var currentTime = moment().format("H");
-        
-        console.log($("textarea")[i])
+        let currentTime = moment().format("H");      
+        let rowTime = text[i].id;
         // test if time block is in the past
-        if(sum > currentTime) {
-            // text.add(".past");
+        if(rowTime < currentTime) {
+            text.addClass("past");
         }    
     }
 }
