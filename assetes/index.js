990ec1ac11920b498c695b88323d402519975fd9
alex-h <alexanderohernandez21@gmail.com>
2022-10-05 17:32:42 -0700

    looping through textarea time block

--- assetes/index.js
+++ assetes/index.js
@@ -1,54 +1,27 @@
-// function time(){
-//     $("#currentDay").text(moment().format("dddd,MMMM Do"));
-    
-//     $(".time-block").each(function(){
-//         var blockText =$(this).attr("id");
-//         $(".text").text(localStorage.getItem(moment().format("MMM Do YYYY")));
-
-//     });
-//     $(".saveBtn").on("click", setLocal);
-
-// };
-
-// var timeBlockEl = $(".col-8");
-// var checkTime = moment().format("H");
-
-// function checkBlock(){
-    
-//     var timeBlockEl = $(".col-8")
-//     for(var i = 0; i< timeBlockEl.length; i++){
-//         var tArea=timeBlockEl[i].id;
-//         var changeColour = document.getElementById(timeBlockEl[i].id);
-//         // removes old classes
-//         $(timeBlockEl[i].id).removeClass(".present .past .future");
-
-//         if(tArea < checkTime){
-//             $(changeColour).addClass("past");
-//         }else if(tArea > checkTime){
-//             changeColour.addClass("future");
-//         }else{
-//             changeColour.addClass("present");
-//         };
-//     } ;
-// };
-
-// function setLocal(){
-//     var text = $(this).parent().attr("id")
-//     localStorage.setItem(moment().format("MMM Do YYYY"), + text, $("textarea").val());
-//     return;
-// }
-
-// // checks  every ten mins 
-// setInterval(checkBlock(),(600000));
-// checkBlock();
-// time();
-
+// displays the header date
 function time() {
     $("#currentDay").text(moment().format("dddd,MMMM Do"));
 }
 
+// checks time for each text box
+function checkTime() {
+    var sum;
+    for(let i = 0; i < $("textarea").length; i++) {
+        sum = 9 + i;
+        
+        // gives military time 
+        var currentTime = moment().format("H");
+        
+        console.log($("textarea")[i])
+        // test if time block is in the past
+        if(sum > currentTime) {
+            // text.add(".past");
+        }    
+    }
+}
 function init() {
     time();
+    checkTime();
 }
 
 init();
