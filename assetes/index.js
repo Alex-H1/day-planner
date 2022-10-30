c150bfe2c4ef10c5c84abed0be91c5400face22b
alex hernandez <alexanderohernandez21@gmail.com>
2022-10-29 18:47:27 -0700

    trouble shooting

--- assetes/index.js
+++ assetes/index.js
@@ -1,27 +1,27 @@
 // displays the header date
 function time() {
-    $("#currentDay").text(moment().format("dddd,MMMM Do"));
+  $("#currentDay").text(moment().format("dddd,MMMM Do"));
 }
 
 // checks time for each text box
 function checkTime() {
-    var sum;
-    for(let i = 0; i < $("textarea").length; i++) {
-        sum = 9 + i;
-        
-        // gives military time 
-        var currentTime = moment().format("H");
-        
-        console.log($("textarea")[i])
-        // test if time block is in the past
-        if(sum > currentTime) {
-            // text.add(".past");
-        }    
+  var sum;
+  for (let i = 0; i < $("textarea").length; i++) {
+    sum = 9 + i;
+
+    // gives military time
+    var currentTime = moment().format("H");
+
+    console.log($("textarea")[i]);
+    // test if time block is in the past
+    if (sum > currentTime) {
+      // text.add(".past");
     }
+  }
 }
 function init() {
-    time();
-    checkTime();
+  time();
+  checkTime();
 }
 
 init();
--- assetes/java.js
+++ assetes/java.js
@@ -1,42 +1,43 @@
-function time(){
-    $("#currentDay").text(moment().format("dddd,MMMM Do"));
-    
-    $(".time-block").each(function(){
-        var blockText =$(this).attr("id");
-        $(".text").text(localStorage.getItem(moment().format("MMM Do YYYY")));
+function time() {
+  $("#currentDay").text(moment().format("dddd,MMMM Do"));
 
-    });
-    $(".saveBtn").on("click", setLocal);
-
-};
+  $(".time-block").each(function () {
+    var blockText = $(this).attr("id");
+    $(".text").text(localStorage.getItem(moment().format("MMM Do YYYY")));
+  });
+  $(".saveBtn").on("click", setLocal);
+}
 var timeBlockEl = $(".text");
 
 var checkTime = moment().format("H");
 console.log(checkTime);
-function checkBlock(){
-    
-    // var timeBlockEl = $(".col-8")
-    for(var i = 0; i< timeBlockEl.length; i++){
-        var tArea=timeBlockEl[i].id;
-        var changeColour = document.getElementById(timeBlockEl[i].id);
-        // removes old classes
-        $(timeBlockEl[i].id).removeClass(".present .past .future");
+function checkBlock() {
+  // var timeBlockEl = $(".col-8")
+  for (var i = 0; i < timeBlockEl.length; i++) {
+    var tArea = timeBlockEl[i].id;
+    var changeColour = document.getElementById(timeBlockEl[i].id);
+    // removes old classes
+    $(timeBlockEl[i].id).removeClass(".present .past .future");
 
-        if(tArea < checkTime){
-            $(changeColour).addClass("past");
-        }else if(tArea > checkTime){
-            $(changeColour).addClass("future");
-        }else{
-            $(changeColour).addClass("present");
-        };
-    } ;
-};
-function setLocal(event){
-    var text = $(this).parent().attr("id")
-    localStorage.setItem(moment().format("MMM Do YYYY"), + text, $("textarea").val());
-    
-    return;
+    if (tArea < checkTime) {
+      $(changeColour).addClass("past");
+    } else if (tArea > checkTime) {
+      $(changeColour).addClass("future");
+    } else {
+      $(changeColour).addClass("present");
+    }
+  }
 }
-setInterval(checkBlock(),600000);
+function setLocal(event) {
+  var text = $(this).parent().attr("id");
+  localStorage.setItem(
+    moment().format("MMM Do YYYY"),
+    +text,
+    $("textarea").val()
+  );
+
+  return;
+}
+setInterval(checkBlock(), 600000);
 checkBlock();
 time();
