a7266b807a85906600b06dec228f2b1b05c387ea
alex-h <alexanderohernandez21@gmail.com>
2022-10-29 18:21:08 -0700

    save plan functin

--- assetes/index.js
+++ assetes/index.js
@@ -1,12 +1,14 @@
 
 let text = $("textarea");
 let btn = $(".saveBtn");
+let boxText;
 
 let currentTime = moment().format("H");
 
 // displays the header date
 function time() {
     $("#currentDay").text(moment().format("dddd,MMMM Do"));
+
 }
 
 // checks time for each text box
@@ -16,26 +18,25 @@ function checkTime() {
         let parseTime = parseInt(currentTime);
         let parseRow = parseInt(text[i].id);
 
-        $(".saveBtn")[i].on('click', function(){
-            let boxText = $(text[i]).val();
+        $(btn[i]).on('click', function(){
+            boxText = $(text[i]).val();
+            savePlan(boxText);
         });
                 
         if(parseRow < parseTime) {
-            console.log(parseTime)
             $(text[i]).toggleClass("past");
-        }
-        if (parseRow == parseTime) {
+        } else if (parseRow == parseTime) {
             $(text[i]).toggleClass("present");
-        } 
-        if (parseRow > parseTime) {
+        } else if (parseRow > parseTime) {
             $(text[i]).toggleClass("future");
         }
+             
     }
 }
 
 
-function savePlan(event) {
-    
+function savePlan(text) {
+     localStorage.setItem("plan", moment().format("MMM Do YY"));
 }
 
 function init() {
